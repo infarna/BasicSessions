fd0cd12df792b7461fc12b506d0e11ac2b66fb5e
Awanish <awanish.infarna@gmail.com>
2020-05-31 13:58:39 +0530

    removing env files

--- basicproject/settings.py
+++ basicproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6dnhxjvt%i^b$%%05f979$fk$zp-a5p@utmum$xe0ch_2ntvyp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['basicsessions.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['basicsessions.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
